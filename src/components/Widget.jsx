ed28afbbc734ef6e85a1906695abadf7a42814bb
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-08-13 12:26:38 +0530

    Project Setup with vite,tailwind,shadcn ui

---
+++ src/components/Widget.jsx
@@ -0,0 +1,9 @@
+import { Button } from "./ui/button";
+
+const Widget = () => {
+  return (
+    <Button className="hover:scale-105 rounded-full shadow-lg">Feedback</Button>
+  );
+};
+
+export default Widget;
